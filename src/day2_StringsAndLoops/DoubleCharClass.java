01f45dfc7cf020b2c2f4bfbe886db62667e8a73a
saimCybertek <saim@cybertekschool.com>
2020-05-09 15:35:05 -0500

    Day 2 - Strings and Loops

--- src/day2_StringsAndLoops/DoubleCharClass.java
+++ src/day2_StringsAndLoops/DoubleCharClass.java
@@ -1,4 +1,17 @@
 package day2_StringsAndLoops;
 
 public class DoubleCharClass {
+
+    public String doubleChar(String str) {
+        String word = "";
+        for(int i=0; i < str.length(); i++) {
+            //word += "" + str.charAt(i) + str.charAt(i);
+            word += str.substring(i, i+1) + str.substring(i, i+1);
+
+        }
+
+        return word;
+
+    }
+
 }
