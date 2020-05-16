db1275a65f054be925c59f215bedbc1215071489
saimCybertek <saim@cybertekschool.com>
2020-05-16 14:59:13 -0500

    Day 3 - Strings and Loops

--- src/day3_StringsAndLoops/CatDogClass.java
+++ src/day3_StringsAndLoops/CatDogClass.java
@@ -1,11 +1,25 @@
 package day3_StringsAndLoops;
 
 public class CatDogClass {
+    
+    public boolean catDog(String str) {
 
+        int cat = 0;
+        int dog = 0;
 
+        for(int i=0; i < str.length()-2; i++) {
 
+            String word = str.substring(i, i+3);
 
+            if(word.equals("cat")) cat++;
 
+            if(word.equals("dog")) dog++;
+
+        }
+
+        return cat == dog;
+
+    }
 
 
 }
