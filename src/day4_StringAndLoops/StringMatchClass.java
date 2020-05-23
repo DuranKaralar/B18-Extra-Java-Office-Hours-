f8280075a2d1d1f697da16389b97ab001d3e960e
saimCybertek <saim@cybertekschool.com>
2020-05-23 13:55:10 -0500

    Day 4 - Strings and Loops

---
+++ src/day4_StringAndLoops/StringMatchClass.java
@@ -0,0 +1,22 @@
+package day4_StringAndLoops;
+
+public class StringMatchClass {
+
+    public int stringMatch(String a, String b) {
+
+        int min = Math.min(a.length(), b.length());
+        int count = 0;
+
+        for(int i=0; i < min-1; i++) {
+
+            String aa = a.substring(i, i+2);
+            String bb = b.substring(i, i+2);
+
+            if(aa.equals(bb)) count++;
+        }
+
+        return count;
+
+    }
+
+}
