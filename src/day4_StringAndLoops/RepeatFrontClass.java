e1ba0503bc8ac5dbf6c03c55e139e4ec6a934111
saimCybertek <saim@cybertekschool.com>
2020-05-23 15:18:51 -0500

    Day 4 - Strings and Loops

---
+++ src/day4_StringAndLoops/RepeatFrontClass.java
@@ -0,0 +1,19 @@
+package day4_StringAndLoops;
+
+public class RepeatFrontClass {
+
+    public String repeatFront(String str, int n) {
+
+        String word = "";
+
+        for(int i=0; i < n; i++) {
+
+            word += str.substring(0, n - i);
+
+        }
+
+        return word;
+
+    }
+
+}
