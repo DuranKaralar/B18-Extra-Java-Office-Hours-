e2127570e3981b693c13f56752c4c67f94c9098c
saimCybertek <saim@cybertekschool.com>
2020-05-23 13:36:23 -0500

    Day 4 - Strings and Loops

---
+++ src/day4_StringAndLoops/Last2Class.java
@@ -0,0 +1,20 @@
+package day4_StringAndLoops;
+
+public class Last2Class {
+
+    public int last2(String str) {
+
+        if(str.length() < 2) return 0;
+
+        String last = str.substring(str.length() -2);
+        int count = 0;
+
+        for(int i=0; i < str.length() -2; i++) {
+
+            if(str.substring(i, i+2).equals(last)) count++;
+
+        }
+        return count;
+    }
+
+}
