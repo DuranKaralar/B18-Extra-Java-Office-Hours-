477418b845f3b47898df0bc7936ab17efd24a6db
saimCybertek <saim@cybertekschool.com>
2020-06-03 16:00:50 -0500

    Day 5 - Array and ArrayList

--- src/day5_ArraysAndArrayList/CountUpperandLower.java
+++ src/day5_ArraysAndArrayList/CountUpperandLower.java
@@ -18,31 +18,68 @@ public class CountUpperandLower {
         System.out.println(Arrays.deepToString(countAll("The hOusE IS biG how mAnY words")));
 }
 
+//    public static int [][] countAll(String str) {
+//
+//        String [] words = str.split(" ");
+//        int [][] count = new int[words.length][2];
+//        int index = 0;
+//
+//        for(String word :words) {
+//
+//            int upper = 0;
+//            int lower = 0;
+//
+//            for(int i=0; i < word.length(); i++) {
+//
+//                if(Character.isUpperCase(word.charAt(i))){
+//                    upper++;
+//                } else if(Character.isLowerCase(word.charAt(i))){
+//                    lower++;
+//                }
+//
+//            }
+//
+//            int [] each = {upper, lower};
+//            count[index++] = each;
+//        }
+//        return count;
+//    }
+
+
+
     public static int [][] countAll(String str) {
 
         String [] words = str.split(" ");
         int [][] count = new int[words.length][2];
         int index = 0;
 
-        for(String word :words) {
-
-            int upper = 0;
-            int lower = 0;
-
-            for(int i=0; i < word.length(); i++) {
-
-                if(Character.isUpperCase(word.charAt(i))){
-                    upper++;
-                } else if(Character.isLowerCase(word.charAt(i))){
-                    lower++;
-                }
-
-            }
-
-            int [] each = {upper, lower};
-            count[index++] = each;
+        for(String word : words) {
+            count[index++] = countUpperAndLower(word);
         }
         return count;
+    }
+
+    private static int [] countUpperAndLower(String word) {
+
+        int upper = 0;
+        int lower = 0;
+
+        for(int i=0; i < word.length(); i++) {
+
+            if(Character.isUpperCase(word.charAt(i))){
+                upper++;
+            } else if(Character.isLowerCase(word.charAt(i))){
+                lower++;
+            }
+
+        }
+
+        int [] each = {upper, lower};
+
+        return each;
+
+        // return new int[] {upper, lower}
+
     }
 
 
