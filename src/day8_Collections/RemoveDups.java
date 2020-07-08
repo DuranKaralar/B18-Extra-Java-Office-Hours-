8030e08f537c77adf5f29d8effc95cac24b5e588
saimCybertek <saim@cybertekschool.com>
2020-07-08 11:05:25 -0500

    Day 9 - Collections

---
+++ src/day8_Collections/RemoveDups.java
@@ -0,0 +1,13 @@
+package day8_Collections;
+
+public class RemoveDups {
+
+    /*
+    * Create a method that will accept a String
+    * and returns a version that removes duplicate
+    * instances of any character
+    Ex: removeDup("AAABBBCCCD") ==> ABCD
+     */
+
+    
+}
