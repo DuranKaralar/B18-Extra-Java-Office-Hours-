597febfca49758e9ac2fcc30cb2a4cb441cfea7d
saimCybertek <saim@cybertekschool.com>
2020-07-15 11:04:12 -0500

    Day 9 - Collections

--- src/day9_CodingBat/CodingBatTasks.java
+++ src/day9_CodingBat/CodingBatTasks.java
@@ -18,6 +18,35 @@ public class CodingBatTasks {
 
     }
 
-    
+    public Map<String, String> mapAB2(Map<String, String> map) {
+
+        if(map.containsKey("a") && map.containsKey("b")){
+
+            if(map.get("a").equals(map.get("b"))){
+                map.remove("a");
+                map.remove("b");
+            }
+
+        }
+
+        return map;
+
+    }
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+
+        if(map.containsKey("a") && map.containsKey("b")) return map;
+
+        if(map.containsKey("a")){
+            map.put("b", map.get("a"));
+        }
+
+        if(map.containsKey("b")){
+            map.put("a", map.get("b"));
+        }
+
+        return map;
+    }
+
 
 }
