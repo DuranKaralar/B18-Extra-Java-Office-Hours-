e492b69f31617ab03bbabdbfd553ab4a9f6a43a7
saimCybertek <saim@cybertekschool.com>
2020-07-15 10:47:38 -0500

    Day 9 - Collections

---
+++ src/day9_CodingBat/CodingBatTasks.java
@@ -0,0 +1,23 @@
+package day9_CodingBat;
+
+import java.util.Map;
+
+public class CodingBatTasks {
+
+    public Map<String, String> topping3(Map<String, String> map) {
+
+        if(map.containsKey("potato")) {
+            map.put("fries", map.get("potato"));
+        }
+
+        if(map.containsKey("salad")) {
+            map.put("spinach", map.get("salad"));
+        }
+
+        return map;
+
+    }
+
+    
+
+}
