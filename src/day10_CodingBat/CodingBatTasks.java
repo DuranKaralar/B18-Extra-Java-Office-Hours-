2735d6d0deeab9a531f76c0a513799c21cc9908e
saimCybertek <saim@cybertekschool.com>
2020-07-22 11:06:16 -0500

    Day 10 - Collections

---
+++ src/day10_CodingBat/CodingBatTasks.java
@@ -0,0 +1,57 @@
+package day10_CodingBat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodingBatTasks {
+
+
+    public Map<String, String> firstChar(String[] strings) {
+
+        Map<String, String> map = new HashMap<>();
+
+        for(String each: strings) {
+
+            String key = each.substring(0,1);
+
+            if(!map.containsKey(key)){
+                map.put(key, "");
+            }
+
+            map.put(key, map.get(key) + each);
+
+        }
+
+        return map;
+
+    }
+
+
+    public String wordAppend(String[] strings) {
+
+        String result = "";
+        Map<String, Boolean> map = new HashMap<>();
+
+        for(String each: strings) {
+
+            if(map.containsKey(each)) {
+
+                result += each;
+                map.remove(each);
+
+            } else {
+                map.put(each, true);
+            }
+
+        }
+
+        return result;
+
+
+    }
+
+
+
+
+
+}
